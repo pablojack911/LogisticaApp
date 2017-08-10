d44f685606bb7dfcfa70f57c84072e27aceaa2c3
Pablo Pocho Insua <pablojack911@hotmail.com>
2017-08-10 11:01:48 -0300

    AsyncTask en EstadoMercaderiaActivity Creado un AsyncTask para que la interfaz no se cuelgue al momento de consultar la base de datos

--- app/src/main/java/com/dynamicsoftware/pocho/logistica/Vista/EstadoMercaderia/EstadoMercaderiaActivity.java
+++ app/src/main/java/com/dynamicsoftware/pocho/logistica/Vista/EstadoMercaderia/EstadoMercaderiaActivity.java
@@ -1,6 +1,8 @@
 package com.dynamicsoftware.pocho.logistica.Vista.EstadoMercaderia;
 
+import android.app.ProgressDialog;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
@@ -31,31 +33,66 @@ public class EstadoMercaderiaActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estado_mercaderia);
         usuario = SaveSharedPreferences.getUserName(EstadoMercaderiaActivity.this);
+        setTitle(usuario + " - Rechazos");
         controladoraItemFactura = new ControladoraItemFactura(EstadoMercaderiaActivity.this);
         controladoraFacturas = new ControladoraFacturas(EstadoMercaderiaActivity.this);
         mercaderiaEnCamionList = new ArrayList<>();
-        Cursor cursorMercaderiaEnCamion = controladoraItemFactura.obtenerCursorMercaderiaEnCamion(usuario);
-        while (cursorMercaderiaEnCamion.moveToNext())
-        {
-            MercaderiaEnCamion mercaderiaEnCamion = new MercaderiaEnCamion();
-            mercaderiaEnCamion.setArticulo(cursorMercaderiaEnCamion.getString(0));
-            mercaderiaEnCamion.setDescripcion(cursorMercaderiaEnCamion.getString(1));
-            mercaderiaEnCamion.setBultos(cursorMercaderiaEnCamion.getInt(2));
-            mercaderiaEnCamionList.add(mercaderiaEnCamion);
-        }
-        efectivoEntregaTotal = controladoraFacturas.totalEfectivoEntregaTotalParaRendir(usuario);
-        efectivoEntregaParcial = controladoraFacturas.totalEfectivoEntregaParcialParaRendir(usuario);
-        bindUI();
+        new InformacionMercaderiaAsyncTask().execute();
     }
 
     private void bindUI()
     {
-        setTitle(usuario + " - Rechazos");
         tvPesosARendir = (TextView) findViewById(R.id.efectivo_a_rendir);
         double total = efectivoEntregaTotal + efectivoEntregaParcial;
         tvPesosARendir.setText(String.format("$ %.2f", total));
         lvArticulosEnCamion = (ListView) findViewById(R.id.lv_articulos_en_camion);
         MercaderiaEnCamionAdapter adapter = new MercaderiaEnCamionAdapter(EstadoMercaderiaActivity.this, mercaderiaEnCamionList);
         lvArticulosEnCamion.setAdapter(adapter);
+    }
+
+    private class InformacionMercaderiaAsyncTask extends AsyncTask<Void, String, Void>
+    {
+        ProgressDialog progressDialog;
+
+        public InformacionMercaderiaAsyncTask()
+        {
+            progressDialog = new ProgressDialog(EstadoMercaderiaActivity.this);
+            progressDialog.setCancelable(false);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            bindUI();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values)
+        {
+            progressDialog.setMessage(values[0]);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            publishProgress("Buscando información...");
+            Cursor cursorMercaderiaEnCamion = controladoraItemFactura.obtenerCursorMercaderiaEnCamion(usuario);
+            while (cursorMercaderiaEnCamion.moveToNext())
+            {
+                MercaderiaEnCamion mercaderiaEnCamion = new MercaderiaEnCamion();
+                mercaderiaEnCamion.setArticulo(cursorMercaderiaEnCamion.getString(0));
+                mercaderiaEnCamion.setDescripcion(cursorMercaderiaEnCamion.getString(1));
+                mercaderiaEnCamion.setBultos(cursorMercaderiaEnCamion.getInt(2));
+                mercaderiaEnCamionList.add(mercaderiaEnCamion);
+            }
+            efectivoEntregaTotal = controladoraFacturas.totalEfectivoEntregaTotalParaRendir(usuario);
+            efectivoEntregaParcial = controladoraFacturas.totalEfectivoEntregaParcialParaRendir(usuario);
+            publishProgress("Listo.");
+            return null;
+        }
     }
 }
